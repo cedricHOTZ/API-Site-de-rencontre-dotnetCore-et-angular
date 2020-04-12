c62fba7ebac9f1ac54e44a6177447b1a9b02efd9
Cédric Hotz <36566958+cedricHOTZ@users.noreply.github.com>
2020-04-12 22:46:18 +0200

    connection avec angular et affichage des valeurs de la BDD

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+     
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+     private readonly DataContext _context;
+     private readonly ILogger<ValuesController> _logger;
+
+       public ValuesController(DataContext context,ILogger<ValuesController> logger ) {
+         
+         _context = context;
+          _logger = logger;
+        
+     }
+        
+       
+        // GET api/values
+        [HttpGet]
+        public async Task <IActionResult> GetValues()
+        {
+           var values = await _context.Values.ToListAsync();
+           return Ok(values);
+        }
+
+          // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task <IActionResult> GetValues(int id)
+        {
+           var values = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+           return Ok(values);
+        }
+    }
+}
